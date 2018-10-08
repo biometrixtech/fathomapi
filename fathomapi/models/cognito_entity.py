10971e0788346b17ebc8045b04b15dcb48ff3a0c
Stephen Poole <stephen@melon.software>
2018-10-08 16:55:56 +0000

    Typo

--- fathomapi/models/cognito_entity.py
+++ fathomapi/models/cognito_entity.py
@@ -55,7 +55,7 @@ class CognitoEntity(Entity):
 
         ret = []
         for user in res['Users']:
-            obj = cls(user['username'])
+            obj = cls(user['Username'])
             obj._hydrate(user['attributes'])
             ret.append(obj)
 
