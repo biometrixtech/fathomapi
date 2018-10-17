3d445861dea61a31ae5965c6593134c02c538163
Stephen Poole <stephen@melon.software>
2018-10-17 12:36:17 +0000

    Fix missing function, not noticed in plans and users because they define it separately

--- fathomapi/api/converters.py
+++ fathomapi/api/converters.py
@@ -1,10 +1,10 @@
-from utils import validate_uuid4
 from werkzeug.routing import BaseConverter, ValidationError
+import uuid
 
 
 class UuidConverter(BaseConverter):
     def to_python(self, value):
-        if validate_uuid4(str(value)):
+        if _validate_uuid4(str(value)):
             return value
         raise ValidationError()
 
@@ -12,3 +12,14 @@ class UuidConverter(BaseConverter):
         return value
 
     type_name = 'uuid'
+
+
+def _validate_uuid4(uuid_string):
+    try:
+        val = uuid.UUID(uuid_string, version=4)
+        # If the uuid_string is a valid hex code, but an invalid uuid4, the UUID.__init__
+        # will convert it to a valid uuid4. This is bad for validation purposes.
+        return val.hex == uuid_string.replace('-', '')
+    except ValueError:
+        # If it's a value error, then the string is not a valid hex code for a UUID.
+        return False
