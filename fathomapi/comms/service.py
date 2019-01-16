7adb338915a3a45b1976424b6ef64c8a45f808e8
Stephen Poole <stephen@melon.software>
2019-01-16 15:12:47 +0000

    Add jitter option to call_apigateway_async_multi()

--- fathomapi/comms/service.py
+++ fathomapi/comms/service.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import random
 
 from .transport import invoke_lambda_sync, invoke_apigateway_sync, push_sqs_sync
 from ..utils.formatters import format_datetime
@@ -24,30 +25,31 @@ class Service:
         return self.call_apigateway_async_multi([{'method': method, 'endpoint': endpoint, 'body': body}], execute_at)
 
     @xray_recorder.capture('fathomapi.comms.service.call_apigateway_async_multi')
-    def call_apigateway_async_multi(self, calls, execute_at=None):
+    def call_apigateway_async_multi(self, calls, execute_at=None, jitter=0):
         if execute_at is None:
             execute_at = datetime.datetime.now()
         service_token = _get_service_token()
 
-        payloads = [{
-            "path": f"/{self.name}/{self.version}/{call['endpoint'].strip('/')}",
-            "httpMethod": call['method'],
-            "headers": {
-                "Accept": "*/*",
-                "Authorization": service_token,
-                "Content-Type": "application/json",
-                "User-Agent": f"Biometrix/API {self.name}:{self.version}",
-                "X-Execute-At": format_datetime(execute_at),
-                "X-Api-Version": self.version,
-            },
-            "pathParameters": {"endpoint": call['endpoint'].strip('/')},
-            "stageVariables": {"LambdaAlias": self.version},
-            "body": json.dumps(call['body']) if call['body'] is not None else None,
-            "isBase64Encoded": False
-        } for call in calls]
+        for i in range(0, len(calls), 10):
+            jitter_offset = datetime.timedelta(seconds=random.randint(0, jitter))
+            payloads = [{
+                "path": f"/{self.name}/{self.version}/{call['endpoint'].strip('/')}",
+                "httpMethod": call['method'],
+                "headers": {
+                    "Accept": "*/*",
+                    "Authorization": service_token,
+                    "Content-Type": "application/json",
+                    "User-Agent": f"Biometrix/API {self.name}:{self.version}",
+                    "X-Execute-At": format_datetime(execute_at + jitter_offset),
+                    "X-Api-Version": self.version,
+                },
+                "pathParameters": {"endpoint": call['endpoint'].strip('/')},
+                "stageVariables": {"LambdaAlias": self.version},
+                "body": json.dumps(call['body']) if call['body'] is not None else None,
+                "isBase64Encoded": False
+            } for call in calls[i:i+10]]
 
-        for i in range(0, len(payloads), 10):
-            push_sqs_sync(f'{self.name}-{{ENVIRONMENT}}-apigateway-async', payloads[i:i+10], execute_at)
+            push_sqs_sync(f'{self.name}-{{ENVIRONMENT}}-apigateway-async', payloads, execute_at + jitter_offset)
 
     @xray_recorder.capture('fathomapi.comms.service.call_lambda_sync')
     def call_lambda_sync(self, function_name, payload=None):
