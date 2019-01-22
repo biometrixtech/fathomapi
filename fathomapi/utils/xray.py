9d51d02b562606e77de1aa243ca3c45fe670d7c5
Stephen Poole <stephen@melon.software>
2019-01-22 18:10:26 +0000

    Try streaming X-Ray subsegments more aggressively

--- fathomapi/utils/xray.py
+++ fathomapi/utils/xray.py
@@ -1,6 +1,7 @@
-from aws_xray_sdk.core import patch_all, xray_recorder as _xray_recorder
+from aws_xray_sdk.core import patch_all as _patch_all, xray_recorder as _xray_recorder
 from aws_xray_sdk.core.models.trace_header import TraceHeader as _TraceHeader
-patch_all()
+_xray_recorder.configure(streaming_threshold=0)
+_patch_all()
 
 xray_recorder = _xray_recorder
 TraceHeader = _TraceHeader
